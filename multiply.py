729b0d1f32f3cad5431d97cb721225607ea0d169
bakerc27-max <bakerc27@oregonstate.edu>
2026-05-13 17:22:14 -0400

    multiply.py

---
+++ multiply.py
@@ -0,0 +1,12 @@
+# Author: Chloe Baker
+# GitHub username: bakerc27_max
+# Date: 05/01/2026
+# Description: Calculates the product of two positive integers using addition
+
+def multiply(num1, num2):
+    """Return the product of two positive integers using addition."""
+    
+    if num2 == 1:
+        return num1
+    else: 
+        return num1 + multiply(num1, num2 - 1)
